73576e04cef826c63a80f60080f29c8c62d51edd
Glendon Chin <glendonchin@gmail.com>
2022-01-10 21:54:52 -0500

    add nav bar js vars

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let myName = document.getElementById("name");
+let aboutMe = document.getElementById("about-me-nav");
+let currentProjects = document.getElementById("current-projects-nav");
+let futureGoals = document.getElementById("future-goals-nav");
 
 function mouseHover() {
     myName.innerHTML="Chin Glendon";
 }
 
-myName.addEventListener("mouseover", mouseHover);
+function mouseOut() {
+    myName.innerHTML = "Glendon Chin";
+}
+
+aboutMe.addEventListener("mouseover", mouseHover);
+aboutMe.addEventListener("mouseout", mouseOut);
